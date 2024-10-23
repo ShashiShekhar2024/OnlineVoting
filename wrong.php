91db8afc1680ae562d163c32c9680c270843acac
ShashiShekhar2024 <shashi.shekhar2010@gmail.com>
2024-10-23 20:28:54 +0530

    Add files via upload
    source file

---
+++ wrong.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="" />
+	<title>Voting</title>
+<link rel="icon" type="image/gif"  href="images/animated_favicon1.gif" >
+
+<link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+<link href="assets/css/votingapp.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+	<div class="container h-100">
+	<div class="d-flex justify-content-center h-100" id="cont">
+		<div class="user_card" id="login">			
+			<div class="d-flex justify-content-center form_container">
+				<p class="mb-5 pb-lg-2 text-white">Please enter correct user name and password. <a href="index.php"
+					style="color: #112d32;">Click here</a></p>
+			</div>			
+		</div>
+	</div>
+</div>
+<script language="javascript" type="text/javascript" src="assets/js/jquery-3.7.1.min.js"></script>
+<script language="javascript" type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
